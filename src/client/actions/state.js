e45e360692d6256b6b1c466bed9f61ce3a0e5e55
Sean Ho <seanthaoho@gmail.com>
2016-06-16 16:05:26 -0700

    Add stub actions/reducers, test react-ace

---
+++ src/client/actions/state.js
@@ -0,0 +1,8 @@
+export const CHANGE_STATE = 'CHANGE_STATE';
+
+export const changeState = function(nextState) {
+  return {
+    type: CHANGE_STATE,
+    nextState
+  }
+}
--- src/client/reducer.js
+++ src/client/reducer.js
@@ -1,6 +1,10 @@
+import { CHANGE_STATE} from './actions/state.js';
+
 export const state = function(state={}, action) {
   switch (action.type) {
+    case CHANGE_STATE:
+      return action.newState;
     default:
-    return state
+      return state
   }
 }
