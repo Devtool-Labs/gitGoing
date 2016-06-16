e3d9398c26fa3671bdcca09d4cca4016598511ff
Veena Sridhar <v.sridhar91@gmail.com>
2016-06-16 16:22:22 -0700

    Render some dummy data for repos

--- src/client/components/TestComponent.jsx
+++ src/client/components/TestComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import RepositoryView from './RepositoryView.jsx';
 
 export default class TestComponent extends React.Component {
  constructor(props) {
@@ -9,7 +10,7 @@ export default class TestComponent extends React.Component {
  render() {
   return (
     <div>
-      yo.
+      <RepositoryView /> 
     </div>
   )
  } 
