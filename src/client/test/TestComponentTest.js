affcc40cd44c033f968af519e8842e4da81a2807
Sean Ho <seanthaoho@gmail.com>
2016-06-17 15:33:50 -0700

    Add some test infastructure

---
+++ src/client/test/TestComponentTest.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { should } from 'should';
+import { shallow, mount, render } from 'enzyme';
+import TestComponent from '../components/TestComponent.jsx';
+
+describe('framework is working', function() {
+  it('should assert true', function() {
+    should.be.true(true);
+  })
+})
+
