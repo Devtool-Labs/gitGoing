b0048ee27430d39ffff3fe6ce88e7bf38d42e8c5
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-06-17 13:20:40 -0700

    Refactor/clean-up Auth-associated code

--- src/server/routers/gitGoingRouter.js
+++ src/server/routers/gitGoingRouter.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 
 router.route('/auth/github')
   .get(passport.authenticate('github', { scope: [ 'user:email' ] }));
+  // .post()
 
 router.route('/auth/github/callback') 
   .get(passport.authenticate('github', { failureRedirect: '/login' }),
