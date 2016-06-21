79290a351c6481ad98d648da64009f63b4b2dc66
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-06-21 16:58:56 -0700

    Final Auth commit

--- src/server/routers/staticRouter.js
+++ src/server/routers/staticRouter.js
@@ -1,4 +1,5 @@
 var router = require('express').Router();
+const isAuthenticated = require('../util/authentication.js');
 
 module.exports = function(app, redisClient) {
   router.route('/signin')
@@ -22,6 +23,10 @@ module.exports = function(app, redisClient) {
       })
     })
 
+  router.route('/')
+    .get(isAuthenticated, function(req, res) {
+      res.render('index.html');
+    });
+
   app.use('', router);
 } 
-
