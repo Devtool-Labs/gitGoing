a914efcd1fbaa7dd0e7b52baacda8bea6eb91016
Veena Sridhar <v.sridhar91@gmail.com>
2016-06-28 16:46:54 -0700

    Change reducer for recursively getting files

--- src/client/components/BranchingView.jsx
+++ src/client/components/BranchingView.jsx
@@ -33,29 +33,7 @@ export default class BranchingView extends React.Component {
   }
 
   clickFile(event) {
-<<<<<<< 8ee444628c5f7d86aaea118e9b5bc66f251a1bb9
     this.props.getFile(this.roomid, this.props.ui.currentCommitSha, event.target.value);
-=======
-    this.props.getFile(this.props.params.roomid, this.props.ui.currentCommitSha, event.target.value);
-  }
-
-  clickFolder (event) {
-    console.log('inside click folder', event.target.value);
-  }
-
-  clickBackButton () {
-    console.log('back button clicked');
-    console.log('sidebarstack is', this.props.ui.sidebarStack);
-    console.log('the length of the sidebar stack is now', this.props.ui.stackLength);
-    for (var i = 0; i < 3; i++) {
-      this.props.ui.sidebarStack.pop();
-    }
-    console.log('the length of the sidebar stack after looping is', this.props.ui.stackLength);
-  }
- 
-  componentWillReceiveProps(newProps) {
-    console.log('Just received new props!');
->>>>>>> Add children array to each element in fileTree props in branching view
   }
 
   clickFolder (event) {
@@ -101,29 +79,7 @@ export default class BranchingView extends React.Component {
         <div>
           <a href="/logout"><button type="button">Sign out</button></a>
           <button onClick={this.clickBackButton}>Back</button>
-<<<<<<< 8ee444628c5f7d86aaea118e9b5bc66f251a1bb9
           <FileTreeView {...this.props} sha={this.state.sha} recursiveFileTree={this.props.getFileTreeRecursively} fileTree={this.props.fileTree}/>
-=======
-          {this.props.fileTree.fileData.map((fileObj, index) => {
-            console.log('the filetree looks like:', this.props.fileTree);
-            console.log('fileobj is', fileObj);
-            if (fileObj.type === 'tree') {
-              return (
-                <div key={index} onClick={this.clickFolder} value={fileObj.sha}>
-                  <i value={fileObj.sha} className="fa fa-folder-open" aria-hidden="true"></i>
-                  <h5 value={fileObj.sha}>{fileObj.path}</h5>
-                </div>
-              );
-            } else {
-              return (
-                <div key={index}>
-                  <i className="fa fa-file-code-o" aria-hidden="true"></i>
-                  <h5>{fileObj.path}</h5>
-                </div>
-              ); 
-            }
-          })}
->>>>>>> Add children array to each element in fileTree props in branching view
         </div>
       );
     } 
