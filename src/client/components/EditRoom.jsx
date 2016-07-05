d101b180bca5d870787bd3e65db678f266ea67fe
Veena Sridhar <v.sridhar91@gmail.com>
2016-07-05 15:46:56 -0700

    Add notification for another user entering a room

--- src/client/components/EditRoom.jsx
+++ src/client/components/EditRoom.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDom  from 'react-dom';
 import BranchView from './BranchingView.jsx';
 import Editor from './Editor.jsx';
+import RoomNotifications from './RoomNotifications.jsx';
 import _ from 'underscore';
 
 export default class EditRoom extends React.Component {
   constructor(props) {
     super(props);
     props.debugModeOn();
+
     if(_.isEmpty(props.room)) {
       props.getRoom(props.params.roomid);
     }
@@ -19,9 +21,16 @@ export default class EditRoom extends React.Component {
     props.initializeSocket(props.params.roomid);
   }
 
+  componentWillReceiveProps (newProps) {
+    if (this.props.user.username) {
+      newProps.joinRoom(newProps.params.roomid, newProps.user.username);
+    }
+  }
+
   render() {
     return (
       <div>
+        <RoomNotifications {...this.props}/>
         <BranchView {...this.props} roomid={this.props.params.roomid}/>
         <Editor ui={this.props.ui} 
         commit={this.props.commit} 
