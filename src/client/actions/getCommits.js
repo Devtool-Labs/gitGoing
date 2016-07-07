39ccd632e0c376960d26df4ba2f905d81327bb00
Veena Sridhar <v.sridhar91@gmail.com>
2016-07-07 11:15:38 -0700

    Modify commits

--- src/client/actions/getCommits.js
+++ src/client/actions/getCommits.js
@@ -20,7 +20,7 @@ export const commitGetResponse = function(status, data) {
 };
 
 export const get = function(roomid){
-  var apiEndpoint = '/api/room/' + roomid + '/commits'
+  var apiEndpoint = '/api/room/' + roomid + '/commits';
   return fetchHelper.get({
     request: commitGetRequest,
     response: commitGetResponse
